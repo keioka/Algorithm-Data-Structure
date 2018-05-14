9ba59fab9c2d6af664183f0db43ee7c21663f9bf
K <koka0828us@gmail.com>
2018-05-13 19:23:42 -0700

    Add max profit multi

---
+++ Arrays & Strings/max_profit_multi/index.js
@@ -0,0 +1,34 @@
+var maxStockMulti = function(prices) {
+    if (prices.length < 2) {
+        return 0;
+    }
+
+    if (prices.length === 2) {
+        return Math.max(prices[1] - prices[0], 0);
+    }
+
+    var min;
+    var max;
+    var set = [];
+    var i = 0;
+    while (prices.length - 2 > i)  {
+        while (prices[i] > prices[i+1]) {
+            i++;
+        }
+
+        min = prices[i];
+
+        while (prices[i] <= prices[i+1]) {
+            i++;
+        }
+
+        max = prices[i];
+
+        set.push([min, max]);
+    }
+
+    return set.reduce((acc, crr) => acc + (crr[1] - crr[0]), 0);
+};
+
+console.log(maxStockMulti([120, 330, 10, 40, 50, 100, 200]));
+console.log(maxStockMulti([10, 330, 1000, 90, 50, 100, 2000]));
