17a2e4f2d4f4350d8f6cd6dd194787293f955f1d
K <koka0828us@gmail.com>
2018-05-17 14:11:44 -0700

    Fix indentation

--- Linked lists/partition/index.js
+++ Linked lists/partition/index.js
@@ -1,45 +1,45 @@
 var LinkedList = require('../single_linked_list');
 
 function LinkedList() {
-  this.head = null
-  this.length = 0
+    this.head = null
+    this.length = 0
 }
 
 function Node(value) {
-  this.value = value
-  this.next = null
+    this.value = value
+    this.next = null
 }
 
 LinkedList.prototype.add = function(value) {
-  var node = new Node(value)
-  if (!this.head) {
-    this.head = node
-    return value
-  }
-  
-  var currentNode = this.head
+    var node = new Node(value)
+    if (!this.head) {
+        this.head = node
+        return value
+    }
 
-  while (currentNode.next) {
-    currentNode = currentNode.next
-  }
-  
-  currentNode.next = node
-  this.length++
-  return value
+    var currentNode = this.head
+
+    while (currentNode.next) {
+        currentNode = currentNode.next
+    }
+
+    currentNode.next = node
+    this.length++
+        return value
 }
 
 LinkedList.prototype.forEach = function(cb) {
-  var node = this.head
-  while(node) {
-    cb(node.value)
-    node = node.next
-  }
+    var node = this.head
+    while (node) {
+        cb(node.value)
+        node = node.next
+    }
 }
 
 LinkedList.prototype.display = function() {
-  var result = []
-  this.forEach((value) => result.push(value))
-  return result
+    var result = []
+    this.forEach((value) => result.push(value))
+    return result
 }
 
 var list = new LinkedList();
@@ -59,46 +59,46 @@ list.add(2)
 list.add(1)
 
 LinkedList.prototype.partition = function(value) {
-  var node = this.head;
-  var leftRoot;
-  var rightRoot;
-  
-  var leftCur;
-  var rightCur;
+    var node = this.head;
+    var leftRoot;
+    var rightRoot;
 
-  while (node) {
-  	if (node.value < value) {
-  	  
-  	  if (!leftRoot) {
-  	  	leftRoot = new Node(node.value);
-  	  	leftCur = leftRoot;
-  	  } else {
-  	  	leftCur.next = node;
-        leftCur = leftCur.next;
-  	  }
-      
-  	} else {
+    var leftCur;
+    var rightCur;
 
-  	  if (!rightRoot) {
-  	  	rightRoot = new Node(node.value);
-  	  	rightCur = rightRoot;
-  	  } else {
-  	  	rightCur.next = node;
-        rightCur = rightCur.next;
-  	  }
-  
-  	}
-  	node = node.next;
-  }
+    while (node) {
+        if (node.value < value) {
 
-  // Avoid circular linked list
-  rightCur.next = null;
+            if (!leftRoot) {
+                leftRoot = new Node(node.value);
+                leftCur = leftRoot;
+            } else {
+                leftCur.next = node;
+                leftCur = leftCur.next;
+            }
 
-  // connect two linked list
-  leftCur.next = rightRoot;
+        } else {
 
-  // Switch a head to new head.
-  this.head = leftRoot;
+            if (!rightRoot) {
+                rightRoot = new Node(node.value);
+                rightCur = rightRoot;
+            } else {
+                rightCur.next = node;
+                rightCur = rightCur.next;
+            }
+
+        }
+        node = node.next;
+    }
+
+    // Avoid circular linked list
+    rightCur.next = null;
+
+    // connect two linked list
+    leftCur.next = rightRoot;
+
+    // Switch a head to new head.
+    this.head = leftRoot;
 }
 
 console.log(list.display());
